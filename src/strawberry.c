ca0653b32efc3f14284139395d0945c8771e31f3
ybaldaya <ybaldaya@openmailbox.org>
2014-09-28 21:52:59 -0400

    Editing straberry core

--- src/strawberry.c
+++ src/strawberry.c
@@ -5,3 +5,32 @@
 #define _MIT_SOURCE
 #include <X11/Xlib.h>
 #include <X11/XKBlib.h>
+
+// Variables
+static bool_quit;
+
+// Functions
+static void start();
+
+void start(){
+	XEvent ev;
+
+	while(!bool_quit && !XNextEvent(dis,&ev)){
+		if(events[ev.type])
+			events[ev.type](&ev);
+	}
+
+}
+
+
+
+
+int main (){
+
+
+	// start wm
+	start();
+
+	exit(0);
+
+}
